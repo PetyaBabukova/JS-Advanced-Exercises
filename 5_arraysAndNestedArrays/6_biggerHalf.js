1f5150d828a269809c453735f7a81119186e63a4
Petya Babukova <p_babukova@abv.bg>
2024-01-04 16:29:08 +0200

    Bigger Half - done

--- 5_arraysAndNestedArrays/6_biggerHalf.js
+++ 5_arraysAndNestedArrays/6_biggerHalf.js
@@ -1,9 +1,10 @@
 function solve(array) {
     let sorted = array.sort((a, b) => a - b);
-    let half = Math.ceil(array.length / 2);
-    let result = array.slice(array[half]);
+    let half = array.length / 2;
+    let result = sorted.slice(half);
 
-    console.log(result);
+    return result;
 };
 
-solve();
+solve([3, 19, 14, 7, 2, 19, 6]);
+solve([4, 7, 2, 5])
