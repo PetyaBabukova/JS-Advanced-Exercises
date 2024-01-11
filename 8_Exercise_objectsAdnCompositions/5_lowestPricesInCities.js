2ce19c375073c6e741d62d68d4067ecd5c4d81c6
Petya Babukova <p_babukova@abv.bg>
2024-01-11 13:32:38 +0200

    5_lowestPrices - fixed

--- 8_Exercise_objectsAdnCompositions/5_lowestPricesInCities.js
+++ 8_Exercise_objectsAdnCompositions/5_lowestPricesInCities.js
@@ -3,6 +3,7 @@ function solve(input) {
 
     input.forEach(element => {
         let [town, product, price] = element.split(' | ');
+        price = Number(price);
 
         if (!products[product] || products[product].price > price) {
             products[product] = { town, price };
@@ -11,18 +12,27 @@ function solve(input) {
 
     for (let product in products) {
         console.log(`${product} -> ${products[product].price} (${products[product].town})`);
-    }
+    };
 
 };
 
 
+// solve([
+//     'Sample Town | Sample Product | 1000',
+//     'Sample Town | Orange | 2',
+//     'Sample Town | Peach | 1',
+//     'Sofia | Orange | 3',
+//     'Sofia | Peach | 2',
+//     'New York | Sample Product | 1000.1',
+//     'New York | Burger | 10'
+// ]);
+
 solve([
-    'Sample Town | Sample Product | 1000',
-    'Sample Town | Orange | 2',
-    'Sample Town | Peach | 1',
-    'Sofia | Orange | 3',
-    'Sofia | Peach | 2',
-    'New York | Sample Product | 1000.1',
-    'New York | Burger | 10'
-]
-);
+    'Sofia City | Audi | 100000',
+    'Sofia City | BMW | 100000',
+    'Sofia City | Mitsubishi | 10000',
+    'Sofia City | Mercedes | 10000',
+    'Sofia City | NoOffenseToCarLovers | 0',
+    'Mexico City | Audi | 1000',
+    'Mexico City | BMW | 99999'
+])
