75d07c9ea56cad39311a1f5e159624fcacf904f9
Petya Babukova <p_babukova@abv.bg>
2024-01-15 19:44:02 +0200

    Lab 2 - solved

--- 11_DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ 11_DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,16 +1,21 @@
 function deleteByEmail() {
     let emailInputElement  = document.querySelector('input[name="email"]');
     let emialCellElements = document.querySelectorAll('tr td:nth-of-type(2)');
+    let resultElement = document.getElementById('result')
 
     // for (const tdElement of emialCellElements) {
     //     console.log(tdElement.textContent);
     // }
 
     let emialElements = Array.from(emialCellElements);
-    let targetElement = emialElements.find(x => x.textContent == emailInputElement.value);
+    let targetElement = emialElements.find(x => x.textContent == emailInputElement.value.trim());
 
     // console.log(targetElement.parentNode); 
     // targetElement.parentNode.removeChild(targetElement); //Old fasion
-
-    targetElement.remove(); // New way
+    if (targetElement) {
+        targetElement.parentNode.remove(); // New way
+        resultElement.textContent = 'Deleted.'
+    } else {
+        resultElement.textContent = 'Not found.'
+    }
 }
