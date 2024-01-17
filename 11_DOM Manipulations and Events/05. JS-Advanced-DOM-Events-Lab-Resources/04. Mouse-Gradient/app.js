bae1a426fadbd0ac22f4c26d81a14e50077de789
Petya Babukova <p_babukova@abv.bg>
2024-01-17 13:37:53 +0200

    Lab 5 - half solved

--- 11_DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ 11_DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,13 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resuultElement = document.getElementById('result');
+
+    const gradientMousOverHandler = (e) => {
+        // console.log(e.target.offsetWidth);
+        // console.log(e.offsetX);
+        let percent = Math.floor(e.offsetX / e.target.offsetWidth * 100);
+
+        resuultElement.textContent = `${percent}%`
+    }
+    gradientElement.addEventListener('mousemove', gradientMousOverHandler)
 }
